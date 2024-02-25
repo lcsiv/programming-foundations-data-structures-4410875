2562190aa66ae1f445bb6f71f0ff298b2e0f1aad
lcsiv <146095090+lcsiv@users.noreply.github.com>
2024-02-25 20:35:59 -0300

    Update main_Done.py

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,5 @@
-def find_second_smallest(my_list):
-    return 0
+def find_second_smallest(my_list) -> str: 
+    sorted(my_list, reverse = True)
+    return str(my_list[1])
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
